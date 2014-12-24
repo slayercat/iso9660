7c30e209a8eb6e14ed88c0b6971f38791443a862
LinZhe LI <slayercat.subscription@gmail.com>
2014-12-24 10:45:07 +0800

    fix issue for use class ISO9660 directly.

--- iso9660.py
+++ iso9660.py
@@ -23,7 +23,7 @@ class ISO9660(object):
         self._paths = []   #path table
 
         self._url   = url
-        if self._get_sector is None: #it might have been set by a subclass
+        if '_get_sector' not in dir(self) or self._get_sector is None: #it might have been set by a subclass
             self._get_sector = self._get_sector_url if url.startswith('http') else self._get_sector_file
 
         ### Volume Descriptors
